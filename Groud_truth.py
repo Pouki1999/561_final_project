292ebb8ae7ff1cc416a6fc33e3cee9cede1d9ed3
Pouki1999 <gogo.maslyankov@gmail.com>
2021-11-29 19:04:18 -0500

    something!

--- Groud_truth.py
+++ Groud_truth.py
@@ -1,8 +1,15 @@
-import Bio
+from Bio import SeqIO
 
 if __name__ == '__main__':
+    c = 0
+    for record in SeqIO.parse('readsMappingToChr1.fa', 'fasta'):
+        print(record.id)
+        c += len(record)
 
+    print(c)
 
+    for record in SeqIO.parse('GCA_000227135.2_ASM22713v2_genomic.fna', 'fasta'):
+        print(record.id)
 
 
 
