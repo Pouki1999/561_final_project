0ec3b8b8716eef63ea7f3f737ea5eb16537eb59c
sbassa <seraphin.bassas@mail.mcgill.ca>
2021-12-07 20:25:53 -0500

    added kmer counting and solid kmer finding methods

--- Groud_truth.py
+++ Groud_truth.py
@@ -7,37 +7,37 @@ if __name__ == '__main__':
     reads = []
     os.makedirs('reads', exist_ok=True)
     os.makedirs('chromosomes', exist_ok=True)
-    os.makedirs('results', exist_ok=True)
-    n = 0
-    for record in SeqIO.parse('readsMappingToChr1.fa', 'fasta'):
-        reads.append(record)
-        if not os.path.isfile(os.path.join('reads', 'read_' + str(n) + '.fa')):
-            SeqIO.write(record, open(os.path.join('reads', 'read_' + str(n) + '.fa'), 'w'), 'fasta')
-            print('create file for read ' + str(n))
-        n += 1
-    n = 0
-    os.makedirs('chromosomes', exist_ok=True)
-    for record in SeqIO.parse('GCA_000227135.2_ASM22713v2_genomic.fna', 'fasta'):
-        if record.id == 'FR799588.2_Leishmania_donovani,_chromosome_1':
-            chr1 = record
-        if not os.path.isfile(os.path.join('chromosomes', 'chromosome_' + str(n) + '.fa')):
-            SeqIO.write(record, open(os.path.join('chromosomes', 'chromosome_' + str(n) + '.fa'), 'w'), 'fasta')
-            print('create file for chromosome ' + str(n))
-        n += 1
-
-    blastn_path = r"C:\Program Files\NCBI\blast-2.12.0+\bin\blastn.exe"
-    subject_path = r"C:\Users\gogom\PycharmProjects\561_final_project\chromosomes\chromosome_0.fa"
+    os.makedirs('new_results', exist_ok=True)
+    # n = 0
+    # for record in SeqIO.parse('readsMappingToChr1.fa', 'fasta'):
+    #     reads.append(record)
+    #     if not os.path.isfile(os.path.join('reads', 'read_' + str(n) + '.fa')):
+    #         SeqIO.write(record, open(os.path.join('reads', 'read_' + str(n) + '.fa'), 'w'), 'fasta')
+    #         print('create file for read ' + str(n))
+    #     n += 1
+    # n = 0
+    # os.makedirs('chromosomes', exist_ok=True)
+    # for record in SeqIO.parse('GCA_000227135.2_ASM22713v2_genomic.fna', 'fasta'):
+    #     if record.id == 'FR799588.2_Leishmania_donovani,_chromosome_1':
+    #         chr1 = record
+    #     if not os.path.isfile(os.path.join('chromosomes', 'chromosome_' + str(n) + '.fa')):
+    #         SeqIO.write(record, open(os.path.join('chromosomes', 'chromosome_' + str(n) + '.fa'), 'w'), 'fasta')
+    #         print('create file for chromosome ' + str(n))
+    #     n += 1
+    #
+    blastn_path = r"C:/Users/seraphinbassas/Documents/Mcgill 2021-2022/COMP-561/ncbi-blast-2.12.0+/bin/blastn"
+    subject_path = r"C:/Users/seraphinbassas/PycharmProjects/561_final_project/chromosomes/chromosome_0.fa"
     for file in os.listdir('reads'):
         read_name = file.split('.')[0]
-        query_path = r"C:\Users\gogom\PycharmProjects\561_final_project\reads\{}.fa".format(read_name)
-        out_path = r"C:\Users\gogom\PycharmProjects\561_final_project\results\{}.xml".format(read_name)
-        if not os.path.isfile(r"C:\Users\gogom\PycharmProjects\561_final_project\results\{}.xml".format(read_name)):
+        query_path = r"C:/Users/seraphinbassas/PycharmProjects/561_final_project/reads/{}.fa".format(read_name)
+        out_path = r"C:/Users/seraphinbassas/PycharmProjects/561_final_project/new_results/{}.xml".format(read_name)
+        if not os.path.isfile(r"C:/Users/seraphinbassas/PycharmProjects/561_final_project/new_results/{}.xml".format(read_name)):
             cl = NcbiblastnCommandline(cmd=blastn_path, query=query_path, subject=subject_path, out=out_path, outfmt=5)
             cl()
 
     hsp_per_read = {}
-    for result in os.listdir('results'):
-        handle = open(os.path.join('results', result), 'r')
+    for result in os.listdir('new_results'):
+        handle = open(os.path.join('new_results', result), 'r')
         blast_record = NCBIXML.read(handle)
         best = float('inf')
         for hsp in blast_record.alignments[0].hsps:
